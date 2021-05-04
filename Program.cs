d68ea8754c4b8f3f553bc9daf01e3575b18229fb
anggaadypratama <anggaadypratama@outlook.com>
2021-05-05 05:09:01 +0700

    praktikum array

--- Program.cs
+++ Program.cs
@@ -8,11 +8,37 @@ using System;
 namespace ProjectConsole{
     class Program{
 
+        // struct DataNilai{
+        //     public int[] assesment {get; set;};
+        //     public int[] kuis {get; set;};
+        //     public int[] tugas {get; set;};
+        // }
+
         static void Main(string[] args) {
             //object tugas
+            int totalNilai = 0;
+            int[] nilai = new int[3];
+            int[,] dataNilai = new int[3,5];
 
-            PraktikumSatu ps = new PraktikumSatu();
-            ps.modifPerulanganDua();
+            string komponen(int i) => (i == 0 ? "Assessment" : (i == 1 ? "Tugas" : "Kuis"));
+
+            for(int i = 0; i< 3; i++){
+                for(int j = 0; j < 5; j++){
+                    Console.Write($"Masukan Nilai {komponen(i)} ke-{j+1} : ");
+                    dataNilai[i,j] = Convert.ToInt16(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+
+            for(int i = 0; i< (dataNilai.GetUpperBound(0) + 1);i++){
+                for(int j = 0;j < (dataNilai.GetUpperBound(1) + 1); j++){
+                    nilai[i] += dataNilai[i,j]/(dataNilai.GetUpperBound(1) + 1);
+                }
+                Console.WriteLine($"rata-rata nilai {komponen(i)} : {(nilai[i])}");
+            }
+
+            foreach(int total in nilai) totalNilai += total;
+            Console.WriteLine($"total rata-rata nilai : {totalNilai/nilai.Length}");
 
         }
     }
